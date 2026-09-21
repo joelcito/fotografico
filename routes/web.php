4c27ae460390e86ccbdc8109fdca6cfe90c9ee57
joelcito <jjjoelcito123@gmail.com>
2026-09-21 15:49:23 -0400

    se

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    // return view('dashboard');
+    return redirect('home');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
